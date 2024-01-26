2102609ed0e4f45abec78aca156daed8f971bdd3
severo <oliveira.severo@gmail.com>
2024-01-26 17:09:05 -0300

    feat: :rocket: Ajustes no CSS e no Javascript

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,17 @@
 // Alvo
 const menuControlOpen = document.getElementById("menu-control-open");
 const menuControlClose = document.getElementById("menu-control-close");
+const menuClotrolSearch = document.getElementById("submit-search");
 
 // Escutadores
-menuControlOpen.addEventListener("click", togleMenu)
-menuControlClose.addEventListener("click", togleMenu)
+menuControlOpen.addEventListener("click", togleMenu);
+menuControlClose.addEventListener("click", togleMenu);
+menuClotrolSearch.addEventListener("click", togleMenu);
 
 
 function togleMenu(event) {
+  event.preventDefault(); 
+
   document.getElementById("menu-container").classList.toggle("active");
   // document.getElementsByTagName("body")[0].classList.toggle("boqueiaScroll");
 }
