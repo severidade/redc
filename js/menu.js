ab3af48aa447fa6b5b16ca4ab6a93674ae6039b7
severo <oliveira.severo@gmail.com>
2024-01-29 09:17:09 -0300

    feat: :rocket: Versão menu desktop

--- js/menu.js
+++ js/menu.js
@@ -2,16 +2,36 @@
 const menuControlOpen = document.getElementById("menu-control-open");
 const menuControlClose = document.getElementById("menu-control-close");
 const menuClotrolSearch = document.getElementById("submit-search");
+const toggleDivice = document.getElementById("menu-container");
 
 // Escutadores
-menuControlOpen.addEventListener("click", togleMenu);
-menuControlClose.addEventListener("click", togleMenu);
-menuClotrolSearch.addEventListener("click", togleMenu);
+menuControlOpen.addEventListener("click", toggleMenu);
+menuControlClose.addEventListener("click", toggleMenu);
+menuClotrolSearch.addEventListener("click", toggleMenu);
 
 
-function togleMenu(event) {
+window.addEventListener('resize', removeMobileClass);
+window.addEventListener('load', removeMobileClass);
+
+
+function toggleMenu(event) {
   event.preventDefault(); 
 
-  document.getElementById("menu-container").classList.toggle("active");
+  toggleDivice.classList.toggle("active");
   // document.getElementsByTagName("body")[0].classList.toggle("boqueiaScroll");
+}
+
+function removeMobileClass() {
+  if (window.innerWidth > 1000) {
+    toggleDivice.classList.remove("mobile");
+    menuControlOpen.classList.remove("active");
+    toggleDivice.classList.remove("active");
+  } else {
+    if (!menuControlOpen.classList.contains("active")) {
+      menuControlOpen.classList.add("active");
+    }
+    if (!toggleDivice.classList.contains("mobile")) {
+      toggleDivice.classList.add("mobile");
+    }
+  }
 }
