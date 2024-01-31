259722f7baa3f32c92392be90e99a8e5fa03c70e
severo <oliveira.severo@gmail.com>
2024-01-31 20:43:20 -0300

    feat: :rocket: slide correndo para um unico lado
    ainda nao consegui colocar gap entre eles.

--- js/slick.js
+++ js/slick.js
@@ -1,18 +1,36 @@
-window.onload = function() {
-  $('.carousel').slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      prevArrow: false,
-      nextArrow: false,
-      centerMode: true,
-      centerPadding: '40px',
-      responsive: [
-          {
-              breakpoint: 768,
-              settings: {
-                  slidesToShow: 1
-              }
-          }
-      ]
-  });
-};
+// window.onload = function() {
+//   $('.carousel').slick({
+//       slidesToShow: 3,
+//       slidesToScroll: 1,
+//       prevArrow: false,
+//       nextArrow: false,
+//       centerMode: true,
+//       centerPadding: '40px',
+//       responsive: [
+//           {
+//               breakpoint: 768,
+//               settings: {
+//                   slidesToShow: 1
+//               }
+//           }
+//       ]
+//   });
+// };
+// $(document).ready(function(){
+//     $('.carousel').slick({
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//         prevArrow: false,
+//         nextArrow: false,
+//         centerMode: true,
+//         centerPadding: '40px',
+//         responsive: [
+//             {
+//                 breakpoint: 768,
+//                 settings: {
+//                     slidesToShow: 1
+//                 }
+//             }
+//         ]
+//     });
+// });
